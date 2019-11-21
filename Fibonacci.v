5de4a775077cf714a60c4a4c429200dda78c3918
bstassens <49692576+bstassens@users.noreply.github.com>
2019-11-20 18:43:53 -0800

    updated from 8bitworkshop.com

---
+++ Fibonacci.v
@@ -0,0 +1,33 @@
+//Fibonacci(up to the 13th # in sequence) model [0,1,1,2,3,5,8,13,21,34,55,89,144]
+//author: Brady Stassens
+
+`timescale 1ns/1ps
+
+module fib(
+  input wire clk,
+  output reg [7:0] y
+  
+);
+  
+  reg [7:0] tmp;
+  
+  initial
+    begin
+      tmp = 'b00000000;
+    end
+  
+  always @(posedge clk)
+    begin
+      y <= y + tmp;
+      tmp <= y;
+      if(y == 0)
+        begin
+          y <= 1;
+        end
+      if(y == 'b10010000)
+        begin
+          y <= 0;
+        end
+    end
+
+endmodule
