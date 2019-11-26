23e60649301b1bbe6ea19831a61428657f9259c9
bstassens <49692576+bstassens@users.noreply.github.com>
2019-11-25 17:30:56 -0800

    updated from 8bitworkshop.com

--- evenCheck8bit.v
+++ evenCheck8bit.v
@@ -1,4 +1,4 @@
-//8-bit even parity checker
+//8-bit even parity checker(isEven)
 //author: Brady Stassens
 
 `timescale 1ns/1ps
@@ -6,16 +6,8 @@
 module even(
   input reg [7:0] in,
   output wire out
-  
 );
   
-  if(in[0:0] == 0)
-    begin
-      assign out = 1;
-    end
-  else
-    begin
-      assign out = 0;
-    end
+  assign out = ~in[0];
   
 endmodule
