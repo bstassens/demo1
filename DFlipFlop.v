daa4fc4945f185c984face848f9c6f64dad1084b
bstassens <49692576+bstassens@users.noreply.github.com>
2019-12-02 18:19:26 -0800

    updated from 8bitworkshop.com

--- DFlipFlop.v
+++ DFlipFlop.v
@@ -4,23 +4,34 @@
 `timescale 1ns/1ps
 
 module dff(
-  input wire clk, d,
-  output reg q
+  input wire clk, s, r,
+  output reg q, qNot
   
 );
   
   always @(posedge clk)
+    if(r)
+      begin
+        q <= 0;
+      end
+  else if(s)
     begin
-      q <= d;
+      q <= 1;
     end
+  else
+    begin
+      q <= q;
+    end
+  
+  assign qNot = ~q;
   
 endmodule
 
 module dff_top(
-  input clk,
-  output q
+  input clk, s, r,
+  output q, qNot
 );
   
-  dff uut(clk, 0, q);
+  dff uut(clk, 0, 0, q, qNot);
   
 endmodule
