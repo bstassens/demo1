19f8635c998a5f099494baafaef8cd7429710f44
bstassens <49692576+bstassens@users.noreply.github.com>
2019-12-02 18:43:05 -0800

    updated from 8bitworkshop.com

--- abcy.v
+++ abcy.v
@@ -1,10 +1,22 @@
-//
+//Function of y = (a and not b) or (a and not c)
+//author: Brady Stassens
+
+`timescale 1ns/100ps
 
 module func(
   input wire a, b, c,
   output wire y
 );
 
-  y = (a && ~b) || (a && ~c);
+  assign y = (a && ~b) || (a && ~c);
 
+endmodule
+
+module func_top(
+  input a, b, c,
+  output y
+);
+  
+  func uut(1, 1, 1, y);
+  
 endmodule
