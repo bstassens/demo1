f574ac758fc4ccfabd0c194b07232418fc485295
bstassens <49692576+bstassens@users.noreply.github.com>
2019-12-02 18:21:05 -0800

    updated from 8bitworkshop.com

--- DFlipFlop.v
+++ DFlipFlop.v
@@ -1,37 +1,17 @@
 //D flip-flop model
 //author: Brady Stassens
 
-`timescale 1ns/1ps
+`timescale 1ns/100ps
 
-module dff(
-  input wire clk, s, r,
-  output reg q, qNot
-  
+module demo1(
+  input wire clk, d,
+  output reg q
+
 );
-  
+
   always @(posedge clk)
-    if(r)
-      begin
-        q <= 0;
-      end
-  else if(s)
     begin
-      q <= 1;
+      q <= d;
     end
-  else
-    begin
-      q <= q;
-    end
-  
-  assign qNot = ~q;
-  
-endmodule
 
-module dff_top(
-  input clk, s, r,
-  output q, qNot
-);
-  
-  dff uut(clk, 0, 0, q, qNot);
-  
 endmodule
